c4131dc866728474fa74d565d12ee769f55a295e
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-08-07 13:45:27 +0600

    search result generated and console logged

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("i am added");
+console.log("js added");
+
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
